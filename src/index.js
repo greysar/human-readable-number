b38ede02e2807ef204d32ce4115518a7ba97d61a
greysar <sergssd@yandex.com>
2022-01-05 21:16:45 +0300

    feat: add my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    if (number == 0) return "zero";
+
+    let ones = [
+        "",
+        " one",
+        " two",
+        " three",
+        " four",
+        " five",
+        " six",
+        " seven",
+        " eight",
+        " nine",
+        " ten",
+        " eleven",
+        " twelve",
+        " thirteen",
+        " fourteen",
+        " fifteen",
+        " sixteen",
+        " seventeen",
+        " eighteen",
+        " nineteen",
+    ];
+    let tens = [
+        "",
+        "",
+        " twenty",
+        " thirty",
+        " forty",
+        " fifty",
+        " sixty",
+        " seventy",
+        " eighty",
+        " ninety",
+    ];
+    let hundred = " hundred";
+    let strFromNum = ("" + number).split("");
+    let finalNumString;
+
+    let checkLength = function () {
+        return (number + "").length;
+    };
+
+    if (checkLength() == 1) {
+        finalNumString = ones[number];
+    }
+
+    if (checkLength() == 2) {
+        if (number < 20) finalNumString = ones[number];
+        if (number > 19) {
+            finalNumString = `${tens[strFromNum[0]]}${ones[strFromNum[1]]}`;
+        }
+    }
+
+    if (checkLength() == 3) {
+        let numStr;
+        finalNumString = `${ones[strFromNum[0]]}${hundred}`;
+
+        let twoNums = strFromNum.slice(1).join("");
+
+        if (twoNums.length == 2) {
+            if (+twoNums < 20) numStr = ones[+twoNums];
+            if (+twoNums > 19) {
+                numStr = `${tens[strFromNum[1]]}${ones[strFromNum[2]]}`;
+            }
+        }
+
+        finalNumString = finalNumString + numStr;
+    }
+
+    return finalNumString.trim();
+};
